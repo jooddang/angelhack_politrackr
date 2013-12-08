acabf4815a27ce575479198a148f9e4b9f617707
jooddang <jooddang@gmail.com>
2013-12-07 21:39:08 -0800

    profile page

---
+++ assets/linker/js/profile.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	if ($('#transaction_history').length === 0) {
+		return false;
+	}
+	$.get('/transaction/find/'
+		, {
+			account : 'asdf@asdf.com'
+		}, function (data) {
+			data.forEach(function (entry) {
+				console.log('each data = ', entry);
+				var result = entry.result;
+				var choice = entry.choice;
+				$.get('/issue/find'
+					, {
+						id: entry.issueId
+					}, function (res) {
+						var title = res.title;
+						var color;
+						if (result === choice) {
+							color = 'win';
+						} else {
+							color = 'lose';
+						}
+						var result_caption;
+						if (result == 0) {
+							result_caption = res.choiceOne;
+						} else if (result == 1) {
+							result_caption = res.choiceTwo;
+						} else {
+							result_caption = 'Not determined yet';
+						}
+						$('#transaction_history').append('<div class=\"' + color + '\">' + title + '&nbsp;' + result_caption + '</div>');
+				});
+			});
+	});
+});
