4c0c8b12ff77cee2ee36dd86e1393274f5895ac3
Sungwoo Kang <bahamut1@gmail.com>
2013-12-08 01:31:59 -0800

    fix error

--- assets/linker/js/bet.js
+++ assets/linker/js/bet.js
@@ -4,5 +4,19 @@ $(document).ready(function() {
 	{
 		console.log(latest);
 	}
+
+	$.ajax("/comment/get", {
+	}).done(function(res) {
+		//console.log(res);
+		//alert("sign up success");
+		//return false;
+		window.location.replace("/signin");
+	}).always(function(res) {
+		//alert("sign up success");
+		//window.location.replace("/signin");
+		//window.location.href = "/signin";
+		//return false;
+	});
+
 });
 
